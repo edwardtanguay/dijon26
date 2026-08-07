116ac5c5b88e15cc2c9a7a7829ff236444f82eb0
Edward Tanguay (windows) <edward@tanguay.info>
2026-08-07 02:15:00 +0200

    ..fix ts

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -130,7 +130,9 @@ function shuffleAndBuildDeck() {
   // Fisher-Yates Shuffle
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1))
-    ;[array[i], array[j]] = [array[j], array[i]]
+    const temp = array[i]!
+    array[i] = array[j]!
+    array[j] = temp
   }
 
   flashcardDeck.value = array.map(item => {
